c5c342ad650519fb8255b947344e8e0fa2cea4bc
Giacomo Pope <giacomopope@gmail.com>
2024-07-26 13:36:47 +0100

    refine shake wrapper and unit test

--- src/dilithium_py/shake/shake_wrapper.py
+++ src/dilithium_py/shake/shake_wrapper.py
@@ -17,7 +17,9 @@ class Shake:
     def __init__(self, algorithm, block_length):
         self.algorithm = algorithm
         self.block_length = block_length
+        self.index = 0
         self.read_blocks = 0
+        self.bytes_left = 0
         self.read_data = b""
 
     def absorb(self, input_bytes):
@@ -25,8 +27,10 @@ class Shake:
         Initialise the XOF with the seed
         and reset other init.
         """
-        self.read_blocks = 0
         self.read_data = b""
+        self.read_blocks = 0
+        self.bytes_left = 0
+        self.index = 0
         self.xof = self.algorithm(input_bytes)
 
     def digest(self, input_bytes, length):
@@ -41,18 +45,39 @@ class Shake:
         Requests n blocks from Shake and stores them
         Ignores any bytes previously read
         """
+        # Because of hashlib we need to request ALL bytes even
+        # if we only want 5 more blocks
         byte_count = self.block_length * (self.read_blocks + n)
         xof_data = self.xof.digest(byte_count)
+
+        # include the extra blocks and remove the read ones
+        self.read_data = (
+            self.read_data[self.index :] + xof_data[-self.block_length * n :]
+        )
         self.read_blocks += n
-        # return last n blocks of bytes
-        self.read_data = xof_data[-self.block_length * n :]
+        self.bytes_left += self.block_length * n
+        self.index = 0
 
     def read(self, n):
+        """
+        Rad n bytes from the XOF
+        """
         # Make sure there are enough bytes to read
-        if n > len(self.read_data):
-            self.get_n_blocks(5 * n)
-        send = self.read_data[:n]
-        self.read_data = self.read_data[n:]
+        if n > self.bytes_left:
+            # If we don't need many bytes, just get 5 blocks
+            if (n - self.bytes_left) < 5 * self.block_length:
+                self.get_n_blocks(5)
+            # Otherwise get as many as we need
+            else:
+                self.get_n_blocks(n // self.block_length + 1)
+
+        # Read from the buffer data the bytes requested
+        send = self.read_data[self.index : self.index + n]
+
+        # Store that we've read the bytes and shift the index
+        self.bytes_left -= n
+        self.index += n
+
         return send
 
 
