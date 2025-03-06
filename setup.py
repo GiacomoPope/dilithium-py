89145ad8e29678f3a03cb7e97a52fbb2eb268424
Giacomo Pope <giacomopope@gmail.com>
2025-03-06 14:58:41 +0000

    add setup.py to help Yocto install

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="dilithium-py",
+    version="1.0.1",
+    python_requires=">=3.9",
+    description="A pure python implementation of ML-DSA (FIPS 204)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Topic :: Security :: Cryptography",
+        "Programming Language :: Python :: 3",
+    ],
+    license="MIT",
+    url="https://github.com/GiacomoPope/dilithium-py",
+    install_requires=["pycryptodome"],
+)
